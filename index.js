8edc9744a7ced0076304314fd6946f4b84b80ea2
Eeo Jun <141bytes@gmail.com>
2019-01-03 12:45:25 +0000

    initial views + template

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+// static content
+app.use(express.static('public'));
+
+// templating
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+function render_page(name, template) {
+    template = template || name;
+    app.get(`/${name}`, (req, res) => res.render(`pages/${template}`, {}));
+}
+
+render_page('', 'index');
 
 app.listen(8080, () => {});
