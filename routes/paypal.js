4b771a571ffcb69344f15c1df627e31ef5cf4856
Eeo Jun <141bytes@gmail.com>
2019-01-06 17:48:46 +0000

    very rough delete lock

--- routes/paypal.js
+++ routes/paypal.js
@@ -1,6 +1,9 @@
 const url = require('url');
+const moment = require('moment');
 const router = require('express').Router();
 const paypal = require('../paypalAPI.js');
+const calendar = require('../calendarAPI.js');
+const utils = require('../utils.js');
 
 
 // Paypal Lock schema
@@ -13,13 +16,31 @@ const paypal = require('../paypalAPI.js');
 
 
 router.get('/ok', (req, res) => {
-    // expect token, paymentId, and PayerId.
+    // expect token, paymentId, and PayerID.
     const { paymentId, PayerID, token } = req.query;
-    if (paymentId && PayerID && token) {
+    if (!(paymentId && PayerID && token)) {
+        res.status(403);
+        res.end();
+        return;
+    }
+
+    const today = moment().startOf('day');
+    calendar.lockClash(calendar.auth, {
+        start: utils.momentToCalendarDate(today),
+        end:   utils.momentToCalendarDate(today.add(1, 'month')),
+        predicate: (_, d) => d.token === token && d.payment_id == paymentId,
+    }, (event, err) => {
         // make sure that the lock still exists and isn't
         // already deleted.
+        if (!event) {
+            res.status(404);
+            res.end();
+            return;
+        }
+        console.log(event);
         paypal.execute_payment(paymentId, PayerID, (err, payment) => {
             // delete paypal lock from calendar
+            calendar.deleteLock(calendar.auth, event.id, (err) => console.error(error));
             if (err) {
                 console.error(err);
                 res.status(500);
@@ -29,7 +50,7 @@ router.get('/ok', (req, res) => {
             res.write("Payment approved.");
             res.end();
         });
-    }
+    });
 });
 
 
@@ -53,11 +74,16 @@ router.get('/payment-demo', (req, res) => {
         }
         console.log(payment);
         const link = payment.links.find(link => link.rel == 'approval_url');
-        const token = url.parse(link.href, {parseQueryString: true}).query.token;
-        console.log({
-            token: token,
+        const desc = JSON.stringify({
+            token: url.parse(link.href, {parseQueryString: true}).query.token,
             payment_id: payment.id,
         });
+        calendar.addLock(calendar.auth, {
+            start:   utils.momentToCalendarDate(moment()),
+            end:     utils.momentToCalendarDate(moment().add(2, 'hours')),
+            summary: 'venue',
+            description: desc,
+        }, (err) => console.log(err))
         res.redirect(link.href);
     });
 });
